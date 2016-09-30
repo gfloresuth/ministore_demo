f8522a1a2b40340264b38dffdc77b3d4d2c02208
gfloresuth <dexterminio@gmail.com>
2016-09-30 16:17:07 -0700

    Edit fixed

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -30,6 +30,36 @@ module.exports.addController = function(app,options){
             });
         }
     });
+    app.post('/product_crud/edit/',function(req,res){
+        var session_exists = !(typeof req.session == 'undefined');
+        if (session_exists==true){
+            session_exists=!(typeof req.session.user == 'undefined');
+        }
+        if (session_exists==false){
+            res.render('noaccess.html');
+        }else{
+            product_id = req.body.txtId;
+            console.log(product_id);
+            new ProductModel().where({'id':product_id})
+            .fetch()
+            .then(function(product){
+                if (product!=null){
+                    product.set({
+                        name:req.body.txtName,
+                        price:req.body.txtPrice
+                    });
+                    product.save();
+                    res.render('product_crud_updated.html');
+                }else{
+                    res.send('Not found');
+                }
+            }).catch(function(error){
+                res.send('Error ' + error);
+
+            });
+        }
+        
+    });
     app.get('/product_crud/edit/:id',function(req,res){
         var session_exists = !(typeof req.session == 'undefined');
         if (session_exists==true){
